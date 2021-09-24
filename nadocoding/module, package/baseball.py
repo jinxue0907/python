b291ddaa5106c1384d7855a93050e785cbb2b142
lasini123 <80078131+lasini123@users.noreply.github.com>
2021-09-24 09:33:28 +0900

    custom_error 생성, raise

---
+++ nadocoding/module, package/baseball.py
@@ -0,0 +1,25 @@
+from baseball_game_engine import make_quiz, check
+from custom_error import InvalidCountError
+
+answer = make_quiz()
+# print(answer)
+#무한반복
+while True:
+# 숫자3자리 중복없이 묻자
+    player = input("숫자 세자리는?")     #player: "123" "fun"
+    try:    # 숫자가 아닐 때 에러 초라
+        player_int = int(player)           #ValueError
+    except ValueError:
+        continue
+    # 길이가 3이 아닐 때 에러 처리
+    if len(player) != 3:
+        raise InvalidCountError("3자리가 아닙니다.")
+# strike, ball 확인하자
+    strike, ball = check(answer, player)
+# 출력하자
+    print(f'{player}\tstrike: {strike}\tball: {ball}')
+# strike가 3일 때, 나가자
+    if strike == 3:
+        break
+# 축하해주자
+print('축하합니다. 짝짝짝👏👏')
