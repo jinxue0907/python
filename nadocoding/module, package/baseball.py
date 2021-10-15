30cd719c75cfa50b50fb453f3f1172a5da87c206
lasini123 <80078131+lasini123@users.noreply.github.com>
2021-10-15 10:11:30 +0900

    top 3, save_history, load_history

--- nadocoding/module, package/baseball.py
+++ nadocoding/module, package/baseball.py
@@ -4,9 +4,31 @@ from custom_error import InvalidCountError
 answer = make_quiz()
 # print(answer)
 #무한반복
+count = 0
+
+def save_history(player, count):
+    with open('baseball_history.txt', 'a') as f:
+        f.write(f'{player}\t{count}\n')
+
+def load_history():
+    count_list = []
+    with open('baseball_history.txt', 'r') as f:
+        while True:
+            line = f.readline()
+            if line == '' or not line:
+                break
+            # print(line.rstrip())
+            line_data = line.rstrip().split('\t')
+            count_list.append(line_data[1])
+    count_list.sort()
+    return count_list[:3]
+
 while True:
 # 숫자3자리 중복없이 묻자
-    player = input("숫자 세자리는?")     #player: "123" "fun"
+    player = input("숫자 세자리는?(t: top3)")     #player: "123" "fun"
+    if player == 't':
+        history = load_history()
+        continue
     try:    # 숫자가 아닐 때 에러 초라
         player_int = int(player)           #ValueError
     except ValueError:
@@ -17,11 +39,14 @@ while True:
         print(f'입력한 숫자의 개수가 정답과 다릅니다.정답: {len(answer)} 글자')
         continue
 # strike, ball 확인하자
+    count += 1
     strike, ball = check(answer, player)
 # 출력하자
-    print(f'{player}\tstrike: {strike}\tball: {ball}')
+    print(f'{player}\tstrike: {strike}\tball: {ball}\t{count}t')
 # strike가 3일 때, 나가자
     if strike == 3:
+        #저장하자
+        save_history(player, count)
         break
 # 축하해주자
 print('축하합니다. 짝짝짝👏👏')
